d989700447a83b9394d221f2ca13f1bbb7fbbe8d
Vishwatej Kashid <vishwatejkashid18@gmail.com>
2025-02-01 17:31:58 +0530

    login page

---
+++ src/components/Auth/login.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+function Login() {
+    const [email ,setEmail] = useState('')
+    const [pass ,setPass] = useState('')
+
+    const submitHandler = (e) =>{
+        e.preventDefault();
+        console.log("email is:",email);
+        console.log("password is:",pass);
+
+        setEmail('');
+        setPass('');
+
+    }
+    return ( 
+        <div className='flex h-screen w-screen items-center justify-center'>
+            <div 
+                className='border-2 rounded-xl border-emerald-600 p-20'>
+                <form 
+                    onSubmit={(e)=>{
+                        submitHandler(e);
+                    }} 
+                    className="flex flex-col items-center justify-center ">
+
+                    <input 
+                        value={email} 
+                        onChange={(e)=>{
+                            setEmail(e.target.value)
+                        }} 
+                        required 
+                        className='outline-none bg-transparent border-2 border-emerald-600 text-xl py-3 px-5 rounded-full placeholder:text-gray-400' 
+                        type="email" 
+                        placeholder="Enter your email"/>
+
+                    <input 
+                        value={pass}
+                        onChange={(e)=>{
+                            setPass(e.target.value)
+                        }} 
+                        required 
+                        className="mt-3 outline-none bg-transparent border-2 border-emerald-600 text-xl py-3 px-5 rounded-full placeholder:text-gray-400" 
+                        type="password" 
+                        placeholder="Enter your password"/>
+
+                    <button 
+                        className=" mt-7 text-white outline-none border-none hover:bg-emerald-700 font-semibold bg-emerald-600 w-full text-lg py-2 px-8 rounded-full ">
+                        Log in
+                    </button>
+
+                </form>
+
+            </div>
+
+        </div>
+     );
+}
+
+export default Login;
